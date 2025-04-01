baf8d6674ccbf1c38c82eee4d599d92eed9ae8df
arthur <perozin.arthur@gmail.com>
2025-04-01 16:02:45 -0300

    corrige exercício 7

--- ExercicioSemana1/exercicioEntrega.hs
+++ ExercicioSemana1/exercicioEntrega.hs
@@ -25,15 +25,12 @@ sumPot2m m n
     | n >= 0 = m * (2^n) + sumPot2m m (n - 1)
     | otherwise = 0
 
-seriePi :: Int -> Double
-seriePi n 
-    | n <= 0    = 0 
-    | even n    = seriePi (n - 1)
-    | otherwise = somaSerie n 1 1 0
+seriePI :: Int -> Double
+seriePI n 
+  | n <= 0 = 0 
+  | otherwise = somaSerie n 1 1 0
 
 somaSerie :: Int -> Int -> Double -> Double -> Double
 somaSerie n i sinal soma
-    | termo < 4 / fromIntegral n = soma
-    | otherwise = somaSerie n (i + 2) (-sinal) (soma + sinal * termo)
-  where
-    termo = 4 / fromIntegral i
+  | 4 / fromIntegral i <= 4 / fromIntegral n = soma
+  | otherwise = somaSerie n (i + 2) (-sinal) (soma + sinal * (4 / fromIntegral i))
--- ExercicioSemana1/exercicio_7.hs
+++ ExercicioSemana1/exercicio_7.hs
@@ -1,12 +1,9 @@
-seriePi :: Int -> Double
-seriePi n 
-    | n <= 0    = 0 
-    | even n    = seriePi (n - 1)
-    | otherwise = somaSerie n 1 1 0
+seriePI :: Int -> Double
+seriePI n 
+  | n <= 0 = 0 
+  | otherwise = somaSerie n 1 1 0
 
 somaSerie :: Int -> Int -> Double -> Double -> Double
 somaSerie n i sinal soma
-    | termo < 4 / fromIntegral n = soma
-    | otherwise = somaSerie n (i + 2) (-sinal) (soma + sinal * termo)
-  where
-    termo = 4 / fromIntegral i
+  | 4 / fromIntegral i <= 4 / fromIntegral n = soma
+  | otherwise = somaSerie n (i + 2) (-sinal) (soma + sinal * (4 / fromIntegral i))
