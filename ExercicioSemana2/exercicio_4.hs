e6a74f75f088cc0f9e6db3682519c47b6e2754e2
artPerozin <perozin.arthur@gmail.com>
2025-04-22 16:01:43 -0300

    Update exercicio_4.hs
    arruma lógica da primeira maneira de resolução do ex 4

--- ExercicioSemana2/exercicio_4.hs
+++ ExercicioSemana2/exercicio_4.hs
@@ -1,12 +1,21 @@
---maneira 1:
-nUltimos :: Int -> [a] -> [a]
-nUltimos 0 _ = []
-nUltimos _ [] = []
-nUltimos n (x:xs) = nUltimos n xs
+-- maneira 1:
+-- nPrimeiros :: Int -> [a] -> [a]
+-- nPrimeiros _ [] = []
+-- nPrimeiros 0 _ = []
+-- nPrimeiros n (x:xs) = x : nPrimeiros (n-1) xs
 
---maneira 2:
+-- inverso :: [a] -> [a]
+-- inverso [] = []
+-- inverso (x:xs) = inverso xs ++ [x]
+
+-- nUltimos :: Int -> [a] -> [a]
+-- nUltimos 0 _ = []
+-- nUltimos _ [] = []
+-- nUltimos n xs = inverso (nPrimeiros (n) (inverso xs))
+
+-- maneira 2:
 -- nUltimos :: Int -> [a] -> [a]
 -- nUltimos n xs = drop (length xs - n) xs
 
---instancia a função a qual recebe um int e uma lista de a
---após isso, dá um drop do comprimento da lista - n  até a sua cauda e retorna
+-- instancia a função a qual recebe um int e uma lista de a
+-- após isso, dá um drop do comprimento da lista - n  até a sua cauda e retorna
