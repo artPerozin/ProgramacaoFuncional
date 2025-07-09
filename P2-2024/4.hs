0f7f04f964947e83c2189c363c8e4b8fe8037540
artPerozin <perozin.arthur@gmail.com>
2025-07-09 19:45:36 -0300

    refina filterVogal do ex4 da p2 2024

--- P2-2024/4.hs
+++ P2-2024/4.hs
@@ -5,9 +5,5 @@ semVogal (x:xs) = filterVogal x : semVogal xs
 filterVogal :: String -> String
 filterVogal [] = []
 filterVogal (x:xs)
-    |x == 'a' = filterVogal xs 
-    |x == 'e' = filterVogal xs
-    |x == 'i' = filterVogal xs
-    |x == 'o' = filterVogal xs
-    |x == 'u' = filterVogal xs
+    | x `elem` "aeiouAEIOU" = filterVogal xs
     |otherwise = [x] ++ filterVogal xs
